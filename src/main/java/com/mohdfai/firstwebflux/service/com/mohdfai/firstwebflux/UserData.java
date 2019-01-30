9e2386a970b426542fdbf276720e65214a501f38
mohdfaizkhan <43747434+mohdfaizkhan@users.noreply.github.com>
2019-01-30 16:17:21 +0530

    initial commit

---
+++ src/main/java/com/mohdfai/firstwebflux/service/com/mohdfai/firstwebflux/UserData.java
@@ -0,0 +1,13 @@
+package com.mohdfai.firstwebflux.service.com.mohdfai.firstwebflux;
+
+public class UserData
+{
+    public String id;
+    public String miles;
+    public String name;
+    public String passport;
+
+    public UserData(String id) {
+        this.id = id;
+    }
+}
